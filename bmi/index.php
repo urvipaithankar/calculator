2cbbbf7546a08ffddc85db0b7922c3db33c03f29
Urvi Paithankar <urvipaithankar@gmail.com>
2017-08-13 16:12:35 +0530

    minor

--- bmi/index.php
+++ bmi/index.php
@@ -1,5 +1,6 @@
 <?php
-$request = explode('/', trim($_SERVER['REQUEST_URI'],'index.php/'));
+echo "Helo";
+$request = explode('/', trim($_SERVER['REQUEST_URI'],'bmi/index.php/'));
 if(isset($request[0]) && isset($request[1]))
 {
     $height = $request[0]/100; // get height in centimeters and convert it to meters [1 m = 100 cm]
@@ -13,7 +14,6 @@ if(isset($request[0]) && isset($request[1]))
 }
 else
 {
-
     echo '"data": {
         "error_code" : "001",
         "error" : "Input missing. Please check the documentation at https://urvipaithankar.github.io/calculator/bmi"
