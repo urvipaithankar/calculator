371fd72c3758ad9a5e1b43708dbd33168f12f766
Urvi Paithankar <urvipaithankar@gmail.com>
2017-08-13 16:51:58 +0530

    gender error

--- bmr/index.php
+++ bmr/index.php
@@ -5,7 +5,7 @@ if(isset($request[0]) == true && isset($request[1]) == true && isset($request[2]
     $height = $request[0]; // get height in centimeters
     $weight = $request[1]; //get weight in kgs
     $age = $request[2]; //get age in years
-    $gender = $request[3]; //get gender [m - male / f - female] 
+    $gender = $request[3]; //get gender 
     $bmr = 0;
 
     if ($gender == "f") 
