b95954b1dee4fa08fcd1b347ab2e0bf9fb9ec9af
Urvi Paithankar <urvipaithankar@gmail.com>
2017-08-13 16:49:42 +0530

    gender missin error added

--- bmr/index.php
+++ bmr/index.php
@@ -12,19 +12,33 @@ if(isset($request[0]) == true && isset($request[1]) == true && isset($request[2]
     {
         $bmr  = 10 * $weight + 6.25 * $height - 5 * $age - 161 ;
         $gender = "female";
-    }
-    else if($gender != "f")
-    {
-        $bmr  = 10 * $weight + 6.25 * $height - 5 * $age + 5 ;
-        $gender = "male";
-    }
-    echo '"data":{ 
+        echo '"data":{ 
         "height" : '.$height.', 
         "weight" : '.$weight.',
         "age" : '.$age.',
-        "gender" : '.$gender.', 
+        "gender" : "'.$gender.'", 
         "bmr" : '.$bmr.'
         }';
+    }
+    else if($gender == "m")
+    {
+        $bmr  = 10 * $weight + 6.25 * $height - 5 * $age + 5 ;
+        $gender = "male";
+        echo '"data":{ 
+        "height" : '.$height.', 
+        "weight" : '.$weight.',
+        "age" : '.$age.',
+        "gender" : "'.$gender.'", 
+        "bmr" : '.$bmr.'
+        }';
+    }
+    else{
+        echo '"data": {
+        "error_code" : "002",
+        "error" : "Gender not specified. Please check the documentation at https://urvipaithankar.github.io/calculator/bmr"
+    }';
+    }
+    
 }
 else
 {
