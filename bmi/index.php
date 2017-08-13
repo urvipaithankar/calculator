9e5159f7a0f20a623413f9f4da52e1a75303d7bf
Urvi Paithankar <urvipaithankar@gmail.com>
2017-08-13 15:57:46 +0530

    Added BMI calculations

---
+++ bmi/index.php
@@ -0,0 +1,22 @@
+<?php
+$request = explode('/', trim($_SERVER['REQUEST_URI'],'bmi/index.php/'));
+if(isset($request[0]) && isset($request[1]))
+{
+    $height = $request[0]/100; // get height in centimeters and convert it to meters [1 m = 100 cm]
+    $weight = $request[1]; //get weight in kgs
+    $bmi = $weight/($height*$height);
+    echo '"data":{
+    "height":'.$height*100.'; 
+    "weight":'.$weight.'; 
+    "bmi":'.$bmi.'; 
+    }';
+}
+else
+{
+
+    echo '"data": {
+        "error_code" : "001",
+        "error" : "Input missing. Please check the documentation at https://urvipaithankar.github.io/calculator/bmi"
+    }';
+}
+?>
