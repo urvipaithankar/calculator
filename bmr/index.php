bb3f95485a0a44327ac7425e44c618ca8ad6e1e5
Urvi Paithankar <urvipaithankar@gmail.com>
2017-08-13 17:10:25 +0530

    bmr update

--- bmr/index.php
+++ bmr/index.php
@@ -1,20 +1,25 @@
 <?php
 $request = explode('/', trim($_SERVER['REQUEST_URI'],'bmr/index.php/'));
-if(isset($request[0]) == true && isset($request[1]) == true && isset($request[2]) == true && isset($request[3]) == true)
+if(isset($request[0]) == true && isset($request[1]) == true && isset($request[2]) == true)
 {
     $height = trim($request[0]); // get height in centimeters
     $weight = trim($request[1]); //get weight in kgs
     $age = trim($request[2]); //get age in years
-    $gender = (string) trim($request[3]); //get gender 
+    
+    if(isset($request[3]))
+        $gender = "female"; //get gender 
+    else
+        $gender = "male";
+
     $bmr  = 10 * $weight + 6.25 * $height - 5 * $age;
 
-    if ($gender == "female") 
+    if ($gender === "female") 
     {
         $bmr -= 161 ;
         $gender = "female";
         
     }
-    else if($gender == "male")
+    else if($gender === "male")
     {
         $bmr  +=  5 ;
         $gender = "male";
